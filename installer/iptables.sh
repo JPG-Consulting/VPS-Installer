c8ca3929b7baf43bfff89448400bf78ff74c97b7
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-01 01:34:09 +0200

    Update iptables.sh

--- installer/iptables.sh
+++ installer/iptables.sh
@@ -10,4 +10,13 @@ if ! is_package_installed iptables-persistent; then
   fi
 fi
 
-
+# Clear IPTables
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
