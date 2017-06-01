59b8fef472afdaaaa4d5cb3608306c6fafbaaf73
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-01 18:00:25 +0200

    Update cron-apt.sh

--- installer/cron-apt.sh
+++ installer/cron-apt.sh
@@ -8,7 +8,7 @@ if ! is_package_installed cron-apt; then
   fi
 fi
 
-sudo cat >/etc/cron-apt/config <<EOL
+cat >/etc/cron-apt/config <<EOL
 # Configuration for cron-apt. For further information about the possible
 # configuration settings see /usr/share/doc/cron-apt/README.gz.
 
@@ -16,4 +16,17 @@ sudo cat >/etc/cron-apt/config <<EOL
 #OPTIONS="-o quiet=1 -o Dir::Etc::SourceList=/etc/apt/sources.list"
 #MAILTO="youremailaddress"
 MAILON="always"
+EOL
+
+if [ ! -f /etc/cron-apt/action.d/5-security ]; then
+  echo "upgrade -y -o APT::Get::Show-Upgraded=True" > /etc/cron-apt/action.d/5-security
+  echo 'OPTIONS="-o quiet=1 -o Dir::Etc::SourceList=/etc/apt/sources.list.d/security.list -o Dir::Etc::SourceParts=\"/dev/null\""' > /etc/cron-apt/config.d/5-security
+fi
+
+cat >/etc/cron-apt/config <<EOL
+#
+# Apt sources.list limited to security update for cron-apt
+#
+deb http://security.debian.org/ wheezy/updates main contrib non-free
+deb-src http://security.debian.org/ wheezy/updates main contrib non-free
 EOL
