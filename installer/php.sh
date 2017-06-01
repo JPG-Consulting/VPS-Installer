f0f0567f41250149e5155e05e1e43f59073f9c73
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-01 16:38:47 +0200

    Update php.sh

--- installer/php.sh
+++ installer/php.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-apt-get --yes install php5 php-pear php5-mysql
+if ! is_package_installed php5; then
+  apt-get --yes install php5
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to install PHP5."
+    exit 1
+  fi
+fi
 
 # Should be tested
 # /etc/php5/apache2/php.ini, /etc/php5/cgi/php.ini, /etc/php5/cli/php.ini
