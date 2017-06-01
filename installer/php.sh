c2ad8289964ebaebb70b20f7c8ce0ff44de77cfa
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-01 16:36:52 +0200

    Update php.sh

--- installer/php.sh
+++ installer/php.sh
@@ -3,6 +3,5 @@
 apt-get --yes install php5 php-pear php5-mysql
 
 # Should be tested
-#sed -i -e "s/^expose_php[[:space:]]*=.*/expose_php = Off/" /etc/php.ini
-sed -i 's/expose_php = On/expose_php = Off/g' /etc/php.ini
-#sed -e 's/expose_php = On/expose_php = Off/g' /etc/php/5.6/fpm/php.ini
+# /etc/php5/apache2/php.ini, /etc/php5/cgi/php.ini, /etc/php5/cli/php.ini
+sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/apache2/php.ini
