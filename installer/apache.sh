5760f16eef2a41bb402fb32144a2c0a0893bbff9
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-01 12:36:59 +0200

    Update apache.sh

--- installer/apache.sh
+++ installer/apache.sh
@@ -1,4 +1,4 @@
-#!ª/bin/bash
+#!/bin/bash
 
 if ! is_package_installed apache2; then
   apt-get --yes install apache2
@@ -8,3 +8,8 @@ if ! is_package_installed apache2; then
   fi
 fi
 
+sed -i 's/^ServerSignature On/ServerSignature Off/g' /etc/apache2/conf.d/security
+sed -i 's/^ServerTokens \(OS\|Full\|Minimal\|Minor\|Major\)/ServerTokens Prod/g' /etc/apache2/conf.d/security
+
+## Write protect Apache, Php, Mysql configuration files
+chattr +i /etc/apache2/apache2.conf
