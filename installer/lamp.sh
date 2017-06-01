8b2359965ffed625a14c91de2dc31124fbd95cc4
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-01 16:40:16 +0200

    Update lamp.sh

--- installer/lamp.sh
+++ installer/lamp.sh
@@ -16,5 +16,15 @@
 # ====================================
 . installer/php.sh
 
+# ====================================
+#  Extra packages bibding them all
+# ====================================
+if ! is_package_installed php5-mysql; then
+  apt-get --yes install php5-mysql
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to install php5-mysql."
+    exit 1
+  fi
+fi
 
 service apache2 restart
