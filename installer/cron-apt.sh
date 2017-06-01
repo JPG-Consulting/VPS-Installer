9d0f30a6016d7be6701e4e6171387cb1e9edb0e7
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-01 17:18:00 +0200

    Update cron-apt.sh

--- installer/cron-apt.sh
+++ installer/cron-apt.sh
@@ -1,1 +1,19 @@
- apt-get --yes install cron-apt
+ #!/bin/bash
+ 
+if ! is_package_installed cron-apt; then
+  apt-get --yes install cron-apt
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to install cron-apt."
+    exit 1
+  fi
+fi
+
+sudo cat >/etc/cron-apt/config <<EOL
+# Configuration for cron-apt. For further information about the possible
+# configuration settings see /usr/share/doc/cron-apt/README.gz.
+
+#APTCOMMAND=/usr/bin/aptitude
+#OPTIONS="-o quiet=1 -o Dir::Etc::SourceList=/etc/apt/sources.list"
+#MAILTO="youremailaddress"
+MAILON="always"
+EOL
