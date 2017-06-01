ab1295ea43ac1fbc205ed919a9af30994f4a4981
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-01 16:52:07 +0200

    Update php.sh

--- installer/php.sh
+++ installer/php.sh
@@ -10,4 +10,23 @@ fi
 
 # Should be tested
 # /etc/php5/apache2/php.ini, /etc/php5/cgi/php.ini, /etc/php5/cli/php.ini
-sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/apache2/php.ini
+if [ -f /etc/php5/apache2/php.ini ]; then
+  sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/apache2/php.ini
+  sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/apache2/php.ini
+fi
+
+if [ -f /etc/php5/cgi/php.ini ]; then
+  sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/cgi/php.ini
+  sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/cgi/php.ini
+fi
+
+if [ -f /etc/php5/cli/php.ini ]; then
+  sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/cli/php.ini
+  sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/cli/php.ini
+fi
+
+if [ -f /etc/php5/fpm/php.ini ]; then
+  sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/fpm/php.ini
+  sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/fpm/php.ini
+fi
+
