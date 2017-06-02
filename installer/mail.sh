614f36df1dc3f6fab34400a39b96f82b903161fe
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-02 12:49:46 +0200

    Update mail.sh

--- installer/mail.sh
+++ installer/mail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# References: https://wiki.debian.org/Postfix#Installing_and_Configuring_Postfix_on_Debian
 
 # ===========================================
 #  Postfix SMTP
@@ -25,12 +25,28 @@ if ! is_package_installed postfix-mysql; then
   fi
 fi
 
+# Don't run postfix until it is secured!
+service postfix stop
+
+# Postfix basic setup
+# Add your domain to the config files, so others can't abuse your mailsystem:
+postconf -e "myorigin = $HOSTNAME"
+# Add your hostname (computer name). (Use command "hostname" at the command-line to display your hostname if not sure.)
+postconf -e "myhostname=$HOSTNAME"
+# Now add the domain names that your system will handle.
+postconf -e "relay_domains = $HOSTNAME"
+
+
 # ===========================================
 #  Dovecot IMAP
 # ===========================================
 
 if ! is_package_installed dovecot-imapd; then
-  apt-get --yes install dovecot-imapd
+  if ! is_package_installed dovecot-common; then
+    apt-get --yes install dovecot-imapd dovecot-common
+  else
+    apt-get --yes install dovecot-imapd
+  fi
   if [ $? -ne 0 ]; then
     echo "Error: Failed to install dovecot-imapd."
     exit 1
