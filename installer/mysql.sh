d241cf5e4732aab77adba581e131fd855e9b3cc1
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-02 14:10:50 +0200

    Update mysql.sh

--- installer/mysql.sh
+++ installer/mysql.sh
@@ -9,9 +9,12 @@ if ! is_package_installed mysql-server; then
 fi
 
 mysql -uroot -p --protocol=tcp << _EOF_
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.db WHERE Db LIKE 'test%';
-FLUSH PRIVILEGES;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 DROP DATABASE IF EXISTS test;
+FLUSH PRIVILEGES;
 _EOF_
 if [ $? -ne 0 ]; then
   echo "WARNING: Failed to secure MySQL"
