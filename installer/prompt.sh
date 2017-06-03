2ccafd19aa28abaefc1a2eb16b504c267f6b0cff
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-04 01:03:05 +0200

    Update prompt.sh

--- installer/prompt.sh
+++ installer/prompt.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 # here we ask for the questions needed to proceed with the system installation
 
+function read_mysql_password {
+  local __resultvar=$1
+  local __password
+  local __password2
+
+  while true; do
+    read -sp "MySQL root password: " __password
+    echo
+
+    if [[ -n "$__password" ]]; then
+      read -sp "Retype MySQL root password: " __password2
+      echo
+
+      [ "$__password" = "$__password2" ] && break
+      echo "Sorry, passwords do not match"
+    else
+      echo "Sorry, password can not be empty"
+    fi
+  done
+
+  if [[ "$__resultvar" ]]; then
+    eval $__resultvar="'$__password'"
+  else
+    echo "$__password"
+  fi
+}
+
+read_mysql_password MYSQL_ROOT_PASSWD
