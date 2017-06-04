3e9040e71c3663c8d5b75dab829de33f57fd5720
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-04 02:17:36 +0200

    Update mail.sh

--- installer/mail.sh
+++ installer/mail.sh
@@ -37,34 +37,6 @@ if is_package_installed mysql-server; then
 CREATE DATABASE vmail;
 GRANT USAGE ON *.* TO vmail@'localhost' IDENTIFIED BY 'vmailpass';
 GRANT ALL PRIVILEGES ON vmail.* TO vmail@'localhost';
-USE vmail;
-DROP TABLE IF EXISTS `virtual_domains`;
-CREATE TABLE `virtual_domains` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `UNIQUE_NAME` (`name`)
-) ENGINE=InnoDB ;
-DROP TABLE IF EXISTS `virtual_aliases`;
-CREATE TABLE `virtual_aliases` (
-  `id` int(11) NOT NULL auto_increment,
-  `domain_id` int(11) NOT NULL,
-  `source` varchar(40) NOT NULL,
-  `destination` varchar(80) NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `domain_id` (`domain_id`),
-  CONSTRAINT `virtual_aliases_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB;
-DROP TABLE IF EXISTS `virtual_users`;
-CREATE TABLE `virtual_users` (
-  `id` int(11) NOT NULL auto_increment,
-  `domain_id` int(11) NOT NULL,
-  `user` varchar(40) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `UNIQUE_EMAIL` (`domain_id`,`user`),
-  CONSTRAINT `virtual_users_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB;
 _EOF_
   
   killall -9 mysqld_safe mysqld
