15feff878872f8650963e067a14befce8504eb2d
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-04 13:37:32 +0200

    Update mail.sh

--- installer/mail.sh
+++ installer/mail.sh
@@ -243,3 +243,6 @@ if [[ ! $(grep -F dovecot /etc/postfix/master.cf) ]]; then
   echo "dovecot   unix  -       n       n       -       -       pipe" >> /etc/postfix/master.cf
   echo "   flags=DRhu user=vmail:vmail argv=/usr/lib/dovecot/dovecot-lda -f ${sender} -d ${recipient}" >> /etc/postfix/master.cf
 fi
+
+service dovecot restart
+service postfix restart
