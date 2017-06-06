cc25bb04f0a61684c789a3885758a002ef76fef4
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-06 11:20:02 +0200

    Update php.sh

--- installer/php.sh
+++ installer/php.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
-if ! is_package_installed php5; then
+function php_install {
   apt-get --yes install php5
   if [ $? -ne 0 ]; then
     echo "Error: Failed to install PHP5."
     exit 1
   fi
-fi
+}
 
-# Should be tested
-# /etc/php5/apache2/php.ini, /etc/php5/cgi/php.ini, /etc/php5/cli/php.ini
-if [ -f /etc/php5/apache2/php.ini ]; then
-  sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/apache2/php.ini
-  sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/apache2/php.ini
-  sed -i 's/date.timezone = .*/date.timezone = "Europe\/Madrid"/g' /etc/php5/apache2/php.ini
-fi
+function php_harden {
+  # Should be tested
+  # /etc/php5/apache2/php.ini, /etc/php5/cgi/php.ini, /etc/php5/cli/php.ini
+  if [ -f /etc/php5/apache2/php.ini ]; then
+    sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/apache2/php.ini
+    sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/apache2/php.ini
+    sed -i 's/date.timezone = .*/date.timezone = "Europe\/Madrid"/g' /etc/php5/apache2/php.ini
+  fi
 
-if [ -f /etc/php5/cgi/php.ini ]; then
-  sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/cgi/php.ini
-  sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/cgi/php.ini
-  sed -i 's/date.timezone = .*/date.timezone = "Europe\/Madrid"/g' /etc/php5/cgi/php.ini
-fi
+  if [ -f /etc/php5/cgi/php.ini ]; then
+    sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/cgi/php.ini
+    sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/cgi/php.ini
+    sed -i 's/date.timezone = .*/date.timezone = "Europe\/Madrid"/g' /etc/php5/cgi/php.ini
+  fi
 
-if [ -f /etc/php5/cli/php.ini ]; then
-  sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/cli/php.ini
-  sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/cli/php.ini
-  sed -i 's/date.timezone = .*/date.timezone = "Europe\/Madrid"/g' /etc/php5/cli/php.ini
-fi
+  if [ -f /etc/php5/cli/php.ini ]; then
+    sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/cli/php.ini
+    sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/cli/php.ini
+    sed -i 's/date.timezone = .*/date.timezone = "Europe\/Madrid"/g' /etc/php5/cli/php.ini
+  fi
 
-if [ -f /etc/php5/fpm/php.ini ]; then
-  sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/fpm/php.ini
-  sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/fpm/php.ini
-  sed -i 's/date.timezone = .*/date.timezone = "Europe\/Madrid"/g' /etc/php5/fpm/php.ini
-fi
-
+  if [ -f /etc/php5/fpm/php.ini ]; then
+    sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/fpm/php.ini
+    sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/fpm/php.ini
+    sed -i 's/date.timezone = .*/date.timezone = "Europe\/Madrid"/g' /etc/php5/fpm/php.ini
+  fi
+}
