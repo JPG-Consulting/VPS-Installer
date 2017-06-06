cce681737ff35b7afbb62c87cac76d5706982b14
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-06 18:37:30 +0200

    Update composer.sh

--- installer/composer.sh
+++ installer/composer.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 function composer_install {
+  local __composer=$(which composer)
   apt-get install --yes curl php5-cli git-core
   if [ $? -ne 0 ]; then
     echo "Error: Failed to install composer requirements."
     exit 1
   fi
   
-  curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+  if [ -z "$__composer" ]; then
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+  fi
 }
