e69abbb99842dc5e026d9cd29996a6b154bd6440
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-06 11:25:36 +0200

    Update php.sh

--- installer/php.sh
+++ installer/php.sh
@@ -9,29 +9,35 @@ function php_install {
 }
 
 function php_harden {
+  local __timezone='Europe/Madrid'
+  
+  if [ -f /etc/timezone ]; then
+    __timezone=$(cat /etc/timezone)
+  fi
+  
   # Should be tested
   # /etc/php5/apache2/php.ini, /etc/php5/cgi/php.ini, /etc/php5/cli/php.ini
   if [ -f /etc/php5/apache2/php.ini ]; then
     sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/apache2/php.ini
     sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/apache2/php.ini
-    sed -i 's/date.timezone = .*/date.timezone = "Europe\/Madrid"/g' /etc/php5/apache2/php.ini
+    sed -i 's/date.timezone = .*/date.timezone = "$__timezone"/g' /etc/php5/apache2/php.ini
   fi
 
   if [ -f /etc/php5/cgi/php.ini ]; then
     sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/cgi/php.ini
     sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/cgi/php.ini
-    sed -i 's/date.timezone = .*/date.timezone = "Europe\/Madrid"/g' /etc/php5/cgi/php.ini
+    sed -i 's/date.timezone = .*/date.timezone = "$__timezone"/g' /etc/php5/cgi/php.ini
   fi
 
   if [ -f /etc/php5/cli/php.ini ]; then
     sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/cli/php.ini
     sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/cli/php.ini
-    sed -i 's/date.timezone = .*/date.timezone = "Europe\/Madrid"/g' /etc/php5/cli/php.ini
+    sed -i 's/date.timezone = .*/date.timezone = "$__timezone"/g' /etc/php5/cli/php.ini
   fi
 
   if [ -f /etc/php5/fpm/php.ini ]; then
     sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/fpm/php.ini
     sed -i 's/mail.add_x_header = On/mail.add_x_header = Off/g' /etc/php5/fpm/php.ini
-    sed -i 's/date.timezone = .*/date.timezone = "Europe\/Madrid"/g' /etc/php5/fpm/php.ini
+    sed -i 's/date.timezone = .*/date.timezone = "$__timezone"/g' /etc/php5/fpm/php.ini
   fi
 }
