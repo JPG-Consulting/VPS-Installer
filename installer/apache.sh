2872847d144c812847958a1e5e07e1302b41d062
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-06 18:24:46 +0200

    Update apache.sh

--- installer/apache.sh
+++ installer/apache.sh
@@ -8,9 +8,9 @@ if ! is_package_installed apache2; then
   fi
 fi
 
-if [ -e /etc/apache2/conf-enabled/security ]; then
-  sed -i 's/^ServerSignature On/ServerSignature Off/g' /etc/apache2/conf-enabled/security
-  sed -i 's/^ServerTokens \(OS\|Full\|Minimal\|Minor\|Major\)/ServerTokens Prod/g' /etc/apache2/conf-enabled/security
+if [ -e /etc/apache2/conf-enabled/security.conf ]; then
+  sed -i 's/^ServerSignature On/ServerSignature Off/g' /etc/apache2/conf-enabled/security.conf
+  sed -i 's/^ServerTokens \(OS\|Full\|Minimal\|Minor\|Major\)/ServerTokens Prod/g' /etc/apache2/conf-enabled/security.conf
 elif [ -e /etc/apache2/conf.d/security ]; then
   sed -i 's/^ServerSignature On/ServerSignature Off/g' /etc/apache2/conf.d/security
   sed -i 's/^ServerTokens \(OS\|Full\|Minimal\|Minor\|Major\)/ServerTokens Prod/g' /etc/apache2/conf.d/security
