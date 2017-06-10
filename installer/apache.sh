cbb3beed078decc83aaeb032de51ce82b65b52c1
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-10 21:50:20 +0200

    Create apache.sh

--- installer/apache.sh
+++ installer/apache.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-if ! is_package_installed apache2; then
+function apache2_install {
   apt-get --yes install apache2
   if [ $? -ne 0 ]; then
     echo "Error: Failed to install apache2."
     exit 1
   fi
-fi
+}
 
-if [ -e /etc/apache2/conf-enabled/security.conf ]; then
-  sed -i 's/^ServerSignature On/ServerSignature Off/g' /etc/apache2/conf-enabled/security.conf
-  sed -i 's/^ServerTokens \(OS\|Full\|Minimal\|Minor\|Major\)/ServerTokens Prod/g' /etc/apache2/conf-enabled/security.conf
-elif [ -e /etc/apache2/conf.d/security ]; then
-  sed -i 's/^ServerSignature On/ServerSignature Off/g' /etc/apache2/conf.d/security
-  sed -i 's/^ServerTokens \(OS\|Full\|Minimal\|Minor\|Major\)/ServerTokens Prod/g' /etc/apache2/conf.d/security
-fi
+function apache2_harden {
+  if [ -e /etc/apache2/conf-enabled/security.conf ]; then
+    sed -i 's/^ServerSignature On/ServerSignature Off/g' /etc/apache2/conf-enabled/security.conf
+    sed -i 's/^ServerTokens \(OS\|Full\|Minimal\|Minor\|Major\)/ServerTokens Prod/g' /etc/apache2/conf-enabled/security.conf
+  elif [ -e /etc/apache2/conf.d/security ]; then
+    sed -i 's/^ServerSignature On/ServerSignature Off/g' /etc/apache2/conf.d/security
+    sed -i 's/^ServerTokens \(OS\|Full\|Minimal\|Minor\|Major\)/ServerTokens Prod/g' /etc/apache2/conf.d/security
+  fi
+}
