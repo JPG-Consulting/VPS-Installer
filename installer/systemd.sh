e98002d4e731675619e03e0590521cc55e6cbc34
JPG-Consulting <opensource@jpg-consulting.es>
2017-06-11 00:25:40 +0200

    Create systemd.sh

--- installer/systemd.sh
+++ installer/systemd.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-apt-get --yes install systemd systemd-sysv
-
+function systemd_install {
+  if ! is_package_installed systemd; then
+    if ! is_package_installed systemd-sysv; then
+      apt-get --yes install systemd systemd-sysv
+      if [ $? -ne 0 ]; then
+        echo "Error: Failed to install systemd."
+        exit 1
+      fi
+    else
+      apt-get --yes install systemd
+      if [ $? -ne 0 ]; then
+        echo "Error: Failed to install systemd."
+        exit 1
+      fi
+    fi
+  elif ! is_package_installed systemd-sysv; then
+    apt-get --yes install systemd-sysv
+    if [ $? -ne 0 ]; then
+      echo "Error: Failed to install systemd."
+      exit 1
+    fi
+  fi
+}
