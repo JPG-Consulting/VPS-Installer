7dfc64dbcc867e1aa6c51602768bc4a2a0e90c49
jpg-consulting <unaputacuenta@hotmail.com>
2017-06-25 10:28:30 +0200

    fix

--- installer/sql/vmail.sql
+++ installer/sql/vmail.sql
@@ -1,13 +1,11 @@
 USE vmail;
-DROP TABLE IF EXISTS `virtual_domains`;
-CREATE TABLE `virtual_domains` (
+CREATE TABLE IF NOT EXISTS `virtual_domains` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `UNIQUE_NAME` (`name`)
 ) ENGINE=InnoDB ;
-DROP TABLE IF EXISTS `virtual_aliases`;
-CREATE TABLE `virtual_aliases` (
+CREATE TABLE IF NOT EXISTS `virtual_aliases` (
   `id` int(11) NOT NULL auto_increment,
   `domain_id` int(11) NOT NULL,
   `source` varchar(40) NOT NULL,
@@ -16,8 +14,7 @@ CREATE TABLE `virtual_aliases` (
   KEY `domain_id` (`domain_id`),
   CONSTRAINT `virtual_aliases_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB;
-DROP TABLE IF EXISTS `virtual_users`;
-CREATE TABLE `virtual_users` (
+CREATE TABLE IF NOT EXISTS `virtual_users` (
   `id` int(11) NOT NULL auto_increment,
   `domain_id` int(11) NOT NULL,
   `user` varchar(40) NOT NULL,
