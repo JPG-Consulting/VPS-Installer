e7a69ed0b92af2942680886999d8cbe88035b468
JPG-Consulting <opensource@jpg-consulting.es>
2018-07-29 18:06:05 +0200

    Update vsftpd.sh

--- installer/vsftpd.sh
+++ installer/vsftpd.sh
@@ -5,4 +5,13 @@ apt-get --yes install vsftpd
 
 useradd --home /home/vsftpd --gid nogroup -m --shell /bin/false vsftpd
 
+sed -i -e 's/anonymous_enable=YES/anonymous_enable=NO/' /etc/vsftpd.conf
+sed -i -e 's/local_enable=NO/local_enable=YES/' /etc/vsftpd.conf
+sed -i -e 's/#chroot_local_user=YES/chroot_local_user=YES/' /etc/vsftpd.conf
+    
+echo "pasv_enable=YES" >> /etc/vsftpd.conf
+echo "pasv_min_port=64000" >> /etc/vsftpd.conf
+echo "pasv_max_port=64321" >> /etc/vsftpd.conf
+echo "port_enable=YES" >> /etc/vsftpd.conf
+    
 service vsftpd restart
